febf7bb1ce1ac5d15bf224b07791774d8aef4473
Adam Ayd <adam.ayd@gmail.com>
2020-10-23 23:35:20 -0400

    declaring variables with primative data types

--- Go_Getting_Started/main.go
+++ Go_Getting_Started/main.go
@@ -4,4 +4,23 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Gophers!")
+
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	firstName := "Adam"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r, im)
 }
