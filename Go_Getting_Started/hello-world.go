2ae518d5cf2cbbd259dabbf9d696312529877d1b
Adam Ayd <adam.ayd@gmail.com>
2020-10-23 22:23:55 -0400

    initial commit

---
+++ Go_Getting_Started/hello-world.go
@@ -0,0 +1,15 @@
+package main
+
+// This is an import block, it allows multiple packages to be imported without
+// repeating the "import" keyword.
+import (
+	"fmt"
+)
+
+/*
+The main function, when part of the main package, identifies the entry point of
+an application.
+*/
+func helloWorld() {
+	fmt.Println("Hello World")
+}
