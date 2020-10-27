2174a322c6757518fbdcc912f8b3cd0fbeaef368
Adam Ayd <adam.ayd@gmail.com>
2020-10-26 22:39:03 -0400

    functions

---
+++ Go_Getting_Started/functions.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, Gophers!")
+  startWebServer()
+}
+
+func startWebServer() {
+  fmt.Println("Starting server...")
+  // do important things
+  fmt.Println("Server started")
+}
