1d13d6908829895b68f797c56569aa2fb0ce96fc
Adam Ayd <aayd@fearless.tech>
2020-12-05 10:24:01 -0500

    Created database package and import mysql driver to project.

--- Create_Web_Services_with_Go/working/inventory-service/main.go
+++ Create_Web_Services_with_Go/working/inventory-service/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"product/database"
 	"product/product"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
-	http.ListenAndServe(":5000", nil)
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
